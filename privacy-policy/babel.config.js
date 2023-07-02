e24a3e1e60ee26cf0b8ba1b3362579ee1f999508
Connor Tyrrell <connor@brewgene.com>
2023-07-02 12:48:00 -0400

    Initial Push

--- privacy-policy/babel.config.js
+++ privacy-policy/babel.config.js
@@ -1,12 +1,7 @@
 module.exports = function(api) {
     api.cache(true);
     return {
-      presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset',  "@babel/preset-react", "@babel/preset-env"],
-      plugins: ['react-native-reanimated/plugin', 'babel-plugin-syntax-jsx', "@babel/plugin-proposal-class-properties"],
-      env: {
-        production: {
-          plugins: ['react-native-paper/babel'],
-        },
-      },
+      presets: ["@babel/preset-react", "@babel/preset-env"],
+      plugins: ['babel-plugin-syntax-jsx', "@babel/plugin-proposal-class-properties"],
     };
   };
