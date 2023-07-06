30952da1afb661846883d33839033fc5c40cd3ff
Connor Tyrrell <connor@brewgene.com>
2023-07-06 17:48:13 -0400

    Added favicon

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,5 @@
 export default function HomePage() {
   return (
-      <h1>This application is for use at and around the Camp Conndigo wedding</h1>
+      <h1>This application is for use at and around Camp Conndigo</h1>
   )
 }
